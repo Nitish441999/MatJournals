228089c1adde3f2307adb9b6ffd08b5ced73f4cf
Nitish Kumar <nitish441999@gmail.com>
2024-08-21 11:46:07 +0530

    some change

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="bg-rose-400 sticky top-0 w-full z-20 shadow-md">
-      <div className="max-w-7xl mx-auto">
+      <div className="w-7xl  mx-6">
         <div className="flex flex-wrap justify-between items-center px-4 py-5">
           <div className="flex items-center gap-3">
             <img
